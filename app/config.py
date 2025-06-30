949e8b045d62fda862e661d85f3dfa3a37ec191e
Hygubline <146880923+Hygubline@users.noreply.github.com>
2025-06-30 11:55:59 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 
 class Settings(BaseModel):
     FIREBASE_CREDENTIALS: str = os.getenv("FIREBASE_CREDENTIALS", "C:/Users/hy126/Desktop/FocusFlow/backend/firebase_credentials.json")
-    FIREBASE_PROJECT_ID: str = os.getenv("FIREBASE_PROJECT_ID", "your-firebase-project-id")
+    FIREBASE_PROJECT_ID: str = os.getenv("FIREBASE_PROJECT_ID", "focusflow-28c52")
     NEO4J_URI: str = "bolt://localhost:7687"
     NEO4J_USER: str = "neo4j"
     NEO4J_PASS: str = "password"
